de5b678387a034d674683d013a41655052f1741e
Jasper Visser <jasper.visser@keylane.com>
2021-04-10 12:27:35 +0200

    c

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,18 @@ async fn main() {
 
     println!("Result: {:#?}", result);
 
-    let uri = "127.0.0.1:9042";
-    let session = SessionBuilder::new().known_node(uri).build().await.unwrap();
-    let result = session.query("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 2};", &[]).await.unwrap();
+    let output = Command::new("docker")
+        .args(&[
+            "exec",
+            "somescylla",
+            "cqlsh",
+            "-f",
+            "./setup.cql",
+        ])
+        .output()
+        .unwrap();
 
-    println!("Query result: {:#?}", result);
+    println!("Query result: {:#?}", output);
 
     let result = Command::new("docker")
         .args(&["exec", "somescylla", "nodetool", "status", "ks"])
