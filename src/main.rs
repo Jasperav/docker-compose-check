ffed659c8953aad7edb7b3445be00b0344964e15
Jasper Visser <jasper.visser@keylane.com>
2021-04-10 10:40:15 +0200

    Added some code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{time, thread};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    // Sleep a little bit so Scylla can start up
+    let ten_millis = time::Duration::from_secs(60 * 3);
+
+    thread::sleep(ten_millis);
+
+    // Now check the nodetool status
+    let result = Command::new("docker")
+        .args(&["exec", "somescylla", "nodetool", "status"])
+        .status()
+        .unwrap();
+
+    println!("Result: {:#?}", result);
 }
